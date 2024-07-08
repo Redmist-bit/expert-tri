efd5a67b69a5aaa385a8d84081c9c1ce8e60f684
endhyarnie <endhyarnie@gmail.com>
2024-07-08 16:44:09 +0800

    add custom_web, animated_snippet

---
+++ custom_web/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'custom_web',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Custom content bar animation',
+    'description': 'Adds a continuous animated content bar to the website.',
+    'author': 'Your Name',
+   'depends': ['base', 'website', 'web_editor', 'animated_snippet'],
+    'data': [
+        'views/snippets/snippets.xml',
+        'views/snippets/a_content_bar_templates.xml',
+        'views/snippets/a_content_service_templates.xml',
+        'views/snippets/a_content_slideshow_testimonial_template.xml',
+    ],
+     'assets': {
+        'web.assets_frontend': [
+            '/custom_web/static/src/css/a_content_bar.css',
+            '/custom_web/static/src/css/a_content_service.css',
+            '/custom_web/static/src/css/a_content_slideshow_testimonial.css',
+            
+        ],
+        # 'web.assets_qweb': ['/custom_web/static/src/js/custom_script.js',]
+     },
+
+
+    'installable': True,
+    'application': False,
+}
